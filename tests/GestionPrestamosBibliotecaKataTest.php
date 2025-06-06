6d59c8b7eae0e98118dccd78e66954483430f418
Eva Isabel Herrero <herrero.139723@e.unavarra.es>
2025-06-06 17:15:04 +0200

    [refactor] separados los tets en arrange, act y assert

--- tests/GestionPrestamosBibliotecaKataTest.php
+++ tests/GestionPrestamosBibliotecaKataTest.php
@@ -20,51 +20,61 @@ class GestionPrestamosBibliotecaKataTest extends TestCase
     public function testListProductsIsEmptyInTheStart(): void
     {
         $nuevoLibroVacio = $this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("");
+
         $this->assertEmpty($nuevoLibroVacio);
     }
+
     /**
      * @test
      */
     public function testPrestarUnNuevoLibro(): void{
         $insertarUnNuevoLibro = $this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("prestar dune");
-        $insertarUnNuevoLibroResultado = "dune x1";
-        $this->assertEquals($insertarUnNuevoLibro,$insertarUnNuevoLibroResultado);
+
+        $this->assertEquals($insertarUnNuevoLibro,"dune x1");
     }
+
     /**
      * @test
      */
     public function testPrestarDosNuevoLibro(): void{
         $insertarDosNuevoLibro = $this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("prestar dune 2");
-        $insertarDosNuevoLibroResultado = "dune x2";
-        $this->assertEquals($insertarDosNuevoLibro,$insertarDosNuevoLibroResultado);
+
+        $this->assertEquals($insertarDosNuevoLibro,"dune x2");
     }
+
     /**
      * @test
      */
     public function testPrestarUnNuevoLibroDeDosTipos(): void{
         $this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("prestar dune");
+
         $insertarUnNuevoLibroDeDosTipos = $this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("prestar fundacion");
-        $insertarUnNuevoLibroDeDosTiposResultado = "dune x1, fundacion x1";
-        $this->assertEquals($insertarUnNuevoLibroDeDosTipos,$insertarUnNuevoLibroDeDosTiposResultado);
+
+        $this->assertEquals($insertarUnNuevoLibroDeDosTipos, "dune x1, fundacion x1");
     }
+
     /**
      * @test
      */
     public function testDevolverUnLibroBorraSoloUnLibro(): void{
         $this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("prestar dune 2");
-        $devolverUnLibro =$this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("devolver dune");
-        $devolverUnLibroResultado = "dune x1";
-        $this->assertEquals($devolverUnLibro,$devolverUnLibroResultado);
+
+        $devolverUnLibro = $this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("devolver dune");
+
+        $this->assertEquals($devolverUnLibro,"dune x1");
     }
+
     /**
      * @test
      */
     public function testDevolverUnLibroNoPrestado(): void{
         $this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("prestar dune 2");
-        $devolverUnLibro =$this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("devolver fundacion");
-        $devolverUnLibroResultado = "El libro indicado no esta en prestamo";
-        $this->assertEquals($devolverUnLibro,$devolverUnLibroResultado);
+
+        $devolverUnLibro = $this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("devolver fundacion");
+
+        $this->assertEquals($devolverUnLibro,"El libro indicado no esta en prestamo");
     }
+
     /**
      * @test
      */
@@ -72,8 +82,9 @@ class GestionPrestamosBibliotecaKataTest extends TestCase
         $this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("prestar dune 2");
         $this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("devolver dune");
         $this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("prestar fundacion 4");
+
         $limpiarBiblioteca = $this->gestionPrestamosBibliotecaKata->gestionarLibrosEnBiblioteca("limpiar");
-        $limpiarBibliotecaResultado = "";
-        $this->assertEquals($limpiarBiblioteca,$limpiarBibliotecaResultado);
+
+        $this->assertEquals($limpiarBiblioteca,"");
     }
 }
